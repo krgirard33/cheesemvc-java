facb7acd9d5a04b547b2371f66861582fef6a3bb
Ken Girard <ken.girard@gmail.com>
2018-05-17 19:58:49 -0500

    Aded in a description for cheese, and am now working on removing cheeses... not that I get why anyone would do such a thing... unless it was a fake entry

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by LaunchCode
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     // Request path: cheese/
     @RequestMapping(value = "")
@@ -37,9 +38,27 @@ public class CheeseController {
 
     // Request path: cheese/add
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
+    public String processAddCheeseForm(@RequestParam String cheeseName, String cheeseDescription) {
 
-        cheeses.add(cheeseName);
+        cheeses.put(cheeseName,cheeseDescription);
+
+        // Redirect to cheese/
+        return "redirect:";
+    }
+
+    // Request path: cheese/remove
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    // Request path: cheese/remove
+    @RequestMapping(value="remove", method = RequestMethod.POST)
+    public String removeCheeseForm(@RequestParam String cheeseName) {
+        cheeses.get(cheeseName);
+        cheeses.remove(cheeseName);
 
         // Redirect to cheese/
         return "redirect:";
