9650a4d4ef2978a8446537a9b55fb607273f7896
Ken Girard <ken.girard@gmail.com>
2018-06-07 14:02:20 -0500

    Started adding in User

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -0,0 +1,18 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    public class User {
+        private String username;
+        private String email;
+        private String password;
+
+
+    }
+
+
+}
