c4b9ac2d5c329e9c73fe58920964b6231796c56d
Ken Girard <ken.girard@gmail.com>
2018-06-07 13:12:48 -0500

    Trying to add the type to edit, and not getting the dropdown to populate. I am getting the type value... but not the enum info

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -2,9 +2,14 @@ package org.launchcode.cheesemvc.controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
 import org.launchcode.cheesemvc.models.CheeseData;
+import org.launchcode.cheesemvc.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.SocketTimeoutException;
 
 /**
  * Created by LaunchCode
@@ -29,6 +34,8 @@ public class CheeseController {
     public String displayAddCheeseForm(Model model) {
 
         model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+        model.addAttribute("cheeseTypes", CheeseType.values());
         // Show add
         return "cheese/add";
     }
@@ -37,7 +44,13 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     // the @ModelAttribute Cheese newCheese part automagicly creates setters for the class
     // based off what comes in
-    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese,
+                                       Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Cheese");
+            // Show add
+            return "cheese/add";
+        }
         CheeseData.add(newCheese);
         // Redirect to cheese/
         return "redirect:";
@@ -66,6 +79,7 @@ public class CheeseController {
     public  String displayEditForm(Model model, @PathVariable int cheeseId) {
         Cheese getACheese = CheeseData.getById(cheeseId);
         model.addAttribute("cheese", getACheese);
+        System.out.print(getACheese);
         // Show cheese/edit
         return "cheese/edit";
     }
@@ -75,6 +89,8 @@ public class CheeseController {
         Cheese cheeseToEdit = CheeseData.getById(cheeseId);
         cheeseToEdit.setName(name);
         cheeseToEdit.setDescription(description);
+        //Added this
+        cheeseToEdit.setType(CheeseType.valueOf(name));
         // Redirect to cheese/
         return "redirect:/cheese";
     }
--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -1,8 +1,19 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Cheese {
+    @NotNull
+    @Size(min=3, max=15)
     private String name;
+
+    @NotNull
+    @Size(min=1, message = "Description must not be empty")
     private String description;
+
+    private CheeseType type;
+
     private int cheeseId;
     private static int nextId = 1;
 
@@ -40,5 +51,13 @@ public class Cheese {
     public void setDescription(String description) {
 
         this.description = description;
+    }
+
+    public CheeseType getType() {
+        return type;
+    }
+
+    public void setType(CheeseType type) {
+        this.type = type;
     }
 }
