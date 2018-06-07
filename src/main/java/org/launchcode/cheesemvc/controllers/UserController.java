97862165a321e2bdae7e1dfa3f51e330a1f2e5fa
Ken Girard <ken.girard@gmail.com>
2018-06-07 14:47:28 -0500

    Starting to add User to CheeseMVC

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -1,18 +1,21 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-    public class User {
-        private String username;
-        private String email;
-        private String password;
 
 
+    @RequestMapping(value="user/add")
+    public String add(Model model) {
+        model.addAttribute("title", "Add User");
+        model.addAttribute(new User());
+
+        return "user/add";
     }
-
 
 }
