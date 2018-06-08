73c480e02f4d17aff6479dd29c08834aa3f57369
Ken Girard <ken.girard@gmail.com>
2018-06-07 19:08:22 -0500

    End of class 3.9

--- src/main/java/org/launchcode/cheesemvc/models/User.java
+++ src/main/java/org/launchcode/cheesemvc/models/User.java
@@ -1,8 +1,21 @@
 package org.launchcode.cheesemvc.models;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotEmpty(message = "Please enter your user name")
+    @Size(int min=5, int max=25)
     private String username;
+
+    @NotEmpty(message = "Please enter your email")
+    @Email
     private String email;
+
+    @NotEmpty(message = "Please enter your password.")
     private String password;
 
     public User() {
