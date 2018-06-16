24960e0689ee9088ba9d3097a894aaaffdd46995
Ken Girard <ken.girard@gmail.com>
2018-06-16 00:29:31 -0500

    Setting up Cheese to handle being hooked to a database, got rid of cheeseId, set up id to be generated by the db

--- src/main/java/org/launchcode/cheesemvc/models/Cheese.java
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -1,9 +1,17 @@
 package org.launchcode.cheesemvc.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Cheese {
+    @Id
+    @GeneratedValue
+    private int id;
+
     @NotNull
     @Size(min=3, max=15)
     private String name;
@@ -14,25 +22,15 @@ public class Cheese {
 
     private CheeseType type;
 
-    private int cheeseId;
-    private static int nextId = 1;
-
     public Cheese(String name, String description) {
-        this();
         this.name = name;
         this.description = description;
     }
 
-    public Cheese() {
-        cheeseId = nextId;
-        nextId++;
-    }
-    public int getCheeseId() {
-        return cheeseId;
-    }
-    public void setCheeseId(int cheeseId) {
+    public Cheese() { }
 
-        this.cheeseId = cheeseId;
+    public int getId() {
+        return id;
     }
 
     public String getName() {
